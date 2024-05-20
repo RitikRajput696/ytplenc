9cd6057b1c5cc58ac1e1adaf93ca400c0ab5463e
RitikRajput696 <rixusinput696@gmail.com>
2024-05-20 19:22:04 +0530

    completed

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,19 +1,34 @@
 import handlePlayList from "@/app/api/route";
-import { useState } from "react";
+import { parse, toSeconds } from "iso8601-duration";
+import humanizeDuration from "humanize-duration";
 
 function Result({ id }) {
   let [vidData, isLoading] = handlePlayList(id);
 
+  function processVidData() {
+    let seconds = 0;
+    for (let i = 0; i < vidData.length; ++i) {
+      seconds += toSeconds(parse(vidData[i]));
+    }
+    const avgDuration = parseInt(seconds / vidData.length);
+    const duration = humanizeDuration(seconds * 1000, {
+      units: ["h", "m", "s"],
+    });
+    const avgDurationHumanise = humanizeDuration(avgDuration * 1000, {
+      units: ["h", "m", "s"],
+    });
+    return (
+      <div>
+        <p>total duration: {duration}</p>
+        <p>video average length: {avgDurationHumanise}</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>hii from result</h1>
-      <ul>
-        {isLoading ? (
-          <li>loading ...</li>
-        ) : (
-          vidData.map((item, i) => <li key={i}>{item}</li>)
-        )}
-      </ul>
+      <div>{isLoading ? <p>loading ...</p> : processVidData()}</div>
     </div>
   );
 }
